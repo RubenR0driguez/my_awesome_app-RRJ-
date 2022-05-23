2da2aa0be998bedb28b80af98117279a54a08937
ruben rodriguez <rotap45r@gmail.com>
2022-05-23 14:48:26 -0400

    changed the console.log to say hello ruben and the rest of the world out there

---
+++ App.js
@@ -0,0 +1,9 @@
+// the purpose of this project is to get experinced to using branches in order to simulate mutliple branch team work 
+const express = require('express')
+const app = express()
+//simple hello world node app
+const APP_PORT = process.env.APP_PORT || 3000; app.get('/',(req, res) =>{
+    res.send('HELLO RUBEN AND THE REST OF THE WORLD OUT THERE!')
+}); app.listen(APP_PORT, () => {
+    console.log(`Running app at port:${APP_PORT}`)
+})
